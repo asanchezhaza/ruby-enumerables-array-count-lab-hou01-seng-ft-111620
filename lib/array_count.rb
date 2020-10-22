e9aab2cb650e23ab7629f24d6f507af476dce0d0
Alan sanchez <asanchezhaza@gmail.com>
2020-10-22 05:24:21 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -10,17 +10,17 @@ def count_strings(array)
 end
 
 def count_empty_strings(array)
- result = 0
+ 
   array.count do |element|
 
    if  element.class == String
      
     if element.length == 0
-      result += 1
+    p  element
     end
    end
    
- result
+ 
 
  
 
